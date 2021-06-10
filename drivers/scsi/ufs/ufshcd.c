e8606b7ad87e26df761ed13fbb01a3cc29b391a2
Alice.Chao <alice.chao@mediatek.com>
2021-06-10 11:55:28 +0800

    [ALPS05728133] scsi: ufs: Export ufshcd_hba_stop
    Remove static of ufshcd_hba_stop to export function.
    
    MTK-Commit-Id: e4e3d601c98aa511cb07b0e8866c5ef3499b5a75
    
    Change-Id: I1b8f1bdd5052408ba88c46e9033f03e4a8c157bc
    Signed-off-by: Alice.Chao <alice.chao@mediatek.com>
    CR-Id: ALPS05728133
    Feature: UFS(Universal Flash Storage)

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -4924,7 +4924,7 @@ out:
  * @hba: per adapter instance
  * @can_sleep: perform sleep or just spin
  */
-static inline void ufshcd_hba_stop(struct ufs_hba *hba, bool can_sleep)
+void ufshcd_hba_stop(struct ufs_hba *hba, bool can_sleep)
 {
 	int err;
 
@@ -4943,6 +4943,7 @@ static inline void ufshcd_hba_stop(struct ufs_hba *hba, bool can_sleep)
 	 */
 	ufshcd_vops_auto_hibern8(hba, false);
 }
+EXPORT_SYMBOL_GPL(ufshcd_hba_stop);
 
 /**
  * MTK PATCH
