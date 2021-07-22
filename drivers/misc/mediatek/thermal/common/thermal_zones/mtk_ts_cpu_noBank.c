580d4884a92228faf5909af43b192670303254fb
jerry-sc.wu <jerry-sc.wu@mediatek.com>
2021-07-23 02:39:39 +0800

    [ALPS05917958] Thermal: add no irq
    [Detail]
    Change susend/resume callback to no irq to avoid
    enter idle mode during suspend/resume flow.
    
    MTK-Commit-Id: 36e0fd63faea1466024d612c971d33e2db9dc6b6
    
    Change-Id: I070fbafb3c653f7c96e54b836e83c0440ba8455e
    Signed-off-by: jerry-sc.wu <jerry-sc.wu@mediatek.com>
    CR-Id: ALPS05917958
    Feature: Thermal Management

--- drivers/misc/mediatek/thermal/common/thermal_zones/mtk_ts_cpu_noBank.c
+++ drivers/misc/mediatek/thermal/common/thermal_zones/mtk_ts_cpu_noBank.c
@@ -1658,14 +1658,14 @@ static void tscpu_thermal_shutdown(struct platform_device *dev)
 
 
 /*tscpu_thermal_suspend spend 1000us~1310us*/
-static int tscpu_thermal_suspend
-(struct platform_device *dev, pm_message_t state)
+static int tscpu_thermal_suspend_noirq(struct device *dev)
 {
 #if !defined(CFG_THERM_NO_AUXADC)
 	int cnt = 0;
 	int temp = 0;
 #endif
 	tscpu_printk("%s, %d\n", __func__, talking_flag);
+
 #if THERMAL_PERFORMANCE_PROFILE
 	struct timeval begin, end;
 	unsigned long val;
@@ -1749,16 +1749,18 @@ static int tscpu_thermal_suspend
 						(end.tv_sec - begin.tv_sec),
 						(end.tv_usec - begin.tv_usec));
 #endif
+
 	return 0;
 }
 
 /*tscpu_thermal_suspend spend 3000us~4000us*/
-static int tscpu_thermal_resume(struct platform_device *dev)
+static int tscpu_thermal_resume_noirq(struct device *dev)
 {
 #if !defined(CFG_THERM_NO_AUXADC)
 	int temp = 0;
 	int cnt = 0;
 #endif
+
 	tscpu_printk("%s, %d\n", __func__, talking_flag);
 
 	g_is_temp_valid = 0;
@@ -1888,20 +1890,25 @@ static int tscpu_thermal_resume(struct platform_device *dev)
 
 	g_tc_resume = 2;	/* set "2", resume finish,can read temp */
 
+
 	return 0;
 }
+
+static const struct dev_pm_ops lvts_pm_ops = {
+	.suspend_noirq = tscpu_thermal_suspend_noirq,
+	.resume_noirq = tscpu_thermal_resume_noirq,
+};
 
 static struct platform_driver mtk_thermal_driver = {
 	.remove = NULL,
 	.shutdown = tscpu_thermal_shutdown,
 	.probe = tscpu_thermal_probe,
-	.suspend = tscpu_thermal_suspend,
-	.resume = tscpu_thermal_resume,
 	.driver = {
 		.name = THERMAL_NAME,
 #ifdef CONFIG_OF
 		.of_match_table = mt_thermal_of_match,
 #endif
+		.pm = &lvts_pm_ops,
 	},
 };
 
