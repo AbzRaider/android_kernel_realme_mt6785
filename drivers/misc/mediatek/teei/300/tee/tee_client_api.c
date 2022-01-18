c5e7cb66e3f05d5572aa0d35e06a4926459d6ff6
huifen wang <huifen.wang@mediatek.com>
2022-01-18 14:48:25 +0800

    [ALPS06493842] TEEI: fix Security Vulnerability
    Fix race condition lead to type confusion issue in teei driver
    
    MTK-Commit-Id: 47b0d0e6426856d7aecf7f6dfca218d10403563c
    
    Signed-off-by: huifen wang <huifen.wang@mediatek.com>
    CR-Id: ALPS06493842
    Feature: Microtrust TEEI
    Change-Id: Iec6eb0a77eaa3b26d0df2b0dffd28d559eb5f606
    (cherry-pick from 0e52831de7b8a0ec2a3270daa9c7d5b5e4ffaa76)

--- drivers/misc/mediatek/teei/300/tee/tee_client_api.c
+++ drivers/misc/mediatek/teei/300/tee/tee_client_api.c
@@ -669,6 +669,11 @@ TEEC_Result TEEC_RegisterSharedMemory(struct TEEC_Context *ctx,
 	dma_buf = dma_buf_get(fd);
 	close(fd);
 
+	if (IS_ERR(dma_buf)) {
+		shm->id = -1;
+		return TEEC_ERROR_GENERIC;
+	}
+
 	if (!dma_buf) {
 		shm->id = -1;
 		return TEEC_ERROR_OUT_OF_MEMORY;
@@ -736,6 +741,11 @@ TEEC_Result TEEC_AllocateSharedMemory(struct TEEC_Context *ctx,
 		return TEEC_ERROR_OUT_OF_MEMORY;
 	dma_buf = dma_buf_get(fd);
 	close(fd);
+
+	if (IS_ERR(dma_buf)) {
+		shm->id = -1;
+		return TEEC_ERROR_GENERIC;
+	}
 
 	if (!dma_buf) {
 		shm->id = -1;
