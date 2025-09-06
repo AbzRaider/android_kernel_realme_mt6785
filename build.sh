50fe379b6fb330c2ec810a33f07afd0e99131908
AbzRaider <99021451+AbzRaider@users.noreply.github.com>
2025-09-07 06:34:09 +0900

    buildscript: Welcome to LPlus?

--- build.sh
+++ build.sh
@@ -6,12 +6,12 @@ rm -rf AnyKernel
 source ~/.bashrc && source ~/.profile
 TANGGAL=$(date +"%Y%m%d-%H")
 export LC_ALL=C && export USE_CCACHE=1
+export KBUILD_BUILD_HOST=android-mtk
+export KBUILD_BUILD_USER="AbzRaider"
 export ARCH=arm64
 if [ ! -d "clang" ]; then
     git clone https://gitlab.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-r547379.git clang --depth=1
 fi
-
-[ -d "out" ] && rm -rf out || mkdir -p out
 
 make O=out ARCH=arm64 nemo_defconfig
 
@@ -19,13 +19,13 @@ PATH="${PWD}/clang/bin:${PATH}" \
 make -j$(nproc --all) O=out \
                       CC="clang" \
                       LLVM=1 \
-                      CONFIG_NO_ERROR_ON_MISMATCH=y
+                      CONFIG_NO_ERROR_ON_MISMATCH=y 2>&1 | tee error.log
 }
 
 function zipping()
 {
-git clone --depth=1 https://github.com/kardebayan/AnyKernel3.git AnyKernel
-cp out/arch/arm64/boot/Image.gz AnyKernel
+git clone --depth=1 https://github.com/AbzRaider/AnyKernel33.git -b RM6785 AnyKernel
+cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
 cd AnyKernel
 zip -r9 Stormbreaker-nemo-${TANGGAL}.zip *
 }
